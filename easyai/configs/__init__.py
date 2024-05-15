04df95da9eae3367354cf1ddf4cb3a18d7835d33
watcher.shen <mlshenkai@163.com>
2024-05-15 17:48:00 +0800

    init

---
+++ easyai/configs/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: watcher
+# @Created Time: 2024/5/15 5:40 PM
+# @File: __init__.py
+# @Email: mlshenkai@163.com
---
+++ setup.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Author: watcher
+# @Created Time: 2024/5/15 5:43 PM
+# @File: setup
+# @Email: mlshenkai@163.com
