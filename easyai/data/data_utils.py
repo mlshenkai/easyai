024001233f616c679627425302938bd2db41556d
watcher.shen <mlshenkai@163.com>
2024-06-20 20:13:14 +0800

    change multimodal dataset

--- easyai/data/data_utils.py
+++ easyai/data/data_utils.py
@@ -1,378 +1,92 @@
-# -*- coding: utf-8 -*-
-# @Author: watcher
-# @Created Time: 2024/6/5 下午3:02
-# @File: data_utils
-# @Email: mlshenkai@163.com
+# Copyright 2024 the LlamaFactory team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from enum import Enum, unique
+from typing import TYPE_CHECKING, Dict, List, Tuple, Union
+
+from datasets import concatenate_datasets, interleave_datasets
+
+from ..extras.logging import get_logger
 
 
-import gzip
-import logging
-import os
-import random as rnd
-import tarfile
-import zipfile
-import cv2
+if TYPE_CHECKING:
+    from datasets import Dataset, IterableDataset
+    from transformers import Seq2SeqTrainingArguments
 
-import decord
-import webdataset as wds
-import numpy as np
-import torch
-from torch.utils.data.dataset import IterableDataset, ChainDataset
-from decord import VideoReader
-from easyai.common.registry import registry
-from easyai.data.datasets.multimodal.base_dataset import ConcatDataset
-from tqdm import tqdm
-
-decord.bridge.set_bridge("torch")
-MAX_INT = registry.get("MAX_INT")
+    from ..hparams import DataArguments
 
 
-def load_video(video_path, n_frms=MAX_INT, height=-1, width=-1, sampling="uniform"):
-    vr = VideoReader(uri=video_path, height=height, width=width)
+logger = get_logger(__name__)
 
-    vlen = len(vr)
-    start, end = 0, vlen
 
-    n_frms = min(n_frms, vlen)
+@unique
+class Role(str, Enum):
+    USER = "user"
+    ASSISTANT = "assistant"
+    SYSTEM = "system"
+    FUNCTION = "function"
+    OBSERVATION = "observation"
 
-    if sampling == "uniform":
-        indices = np.arange(start, end, vlen / n_frms).astype(int)
-    elif sampling == "headtail":
-        indices_h = sorted(rnd.sample(range(vlen // 2), n_frms // 2))
-        indices_t = sorted(rnd.sample(range(vlen // 2, vlen), n_frms // 2))
-        indices = indices_h + indices_t
+
+def infer_max_len(source_len: int, target_len: int, max_len: int, reserved_label_len: int) -> Tuple[int, int]:
+    max_target_len = int(max_len * (target_len / (source_len + target_len)))
+    max_target_len = max(max_target_len, reserved_label_len)
+    max_source_len = max_len - min(max_target_len, target_len)
+    return max_source_len, max_target_len
+
+
+def merge_dataset(
+    all_datasets: List[Union["Dataset", "IterableDataset"]],
+    data_args: "DataArguments",
+    training_args: "Seq2SeqTrainingArguments",
+) -> Union["Dataset", "IterableDataset"]:
+    if len(all_datasets) == 1:
+        return all_datasets[0]
+    elif data_args.mix_strategy == "concat":
+        if data_args.streaming:
+            logger.warning("The samples between different datasets will not be mixed in streaming mode.")
+        return concatenate_datasets(all_datasets)
+    elif data_args.mix_strategy.startswith("interleave"):
+        if not data_args.streaming:
+            logger.warning("We recommend using `mix_strategy=concat` in non-streaming mode.")
+        return interleave_datasets(
+            datasets=all_datasets,
+            probabilities=data_args.interleave_probs,
+            seed=training_args.seed,
+            stopping_strategy="first_exhausted" if data_args.mix_strategy.endswith("under") else "all_exhausted",
+        )
     else:
-        raise NotImplementedError
-
-    # get_batch -> T, H, W, C
-    frms = vr.get_batch(indices).permute(3, 0, 1, 2).float()  # (C, T, H, W)
-
-    return frms
+        raise ValueError("Unknown mixing strategy.")
 
 
-def apply_to_sample(f, sample):
-    ## add check for datasets that return none samples for missing items
-    if sample == None or len(sample) == 0:
-        return {}
-
-    def _apply(x):
-        if torch.is_tensor(x):
-            return f(x)
-        elif isinstance(x, dict):
-            return {key: _apply(value) for key, value in x.items()}
-        elif isinstance(x, list):
-            return [_apply(x) for x in x]
-        else:
-            return x
-
-    return _apply(sample)
-
-
-def move_to_cuda(sample):
-    def _move_to_cuda(tensor):
-        return tensor.cuda()
-
-    return apply_to_sample(_move_to_cuda, sample)
-
-
-def prepare_sample(samples, cuda_enabled=True):
-    if cuda_enabled:
-        samples = move_to_cuda(samples)
-
-    # TODO fp16 support
-
-    return samples
-
-
-def reorg_datasets_by_split(datasets):
-    """
-    Organizes datasets by split.
-
-    Args:
-        datasets: dict of torch.utils.data.Dataset objects by name.
-
-    Returns:
-        Dict of datasets by split {split_name: List[Datasets]}.
-    """
-    # if len(datasets) == 1:
-    #     return datasets[list(datasets.keys())[0]]
-    # else:
-    reorg_datasets = dict()
-
-    # reorganize by split
-    for _, dataset in datasets.items():
-        for split_name, dataset_split in dataset.items():
-            if split_name not in reorg_datasets:
-                reorg_datasets[split_name] = [dataset_split]
+def split_dataset(
+    dataset: Union["Dataset", "IterableDataset"], data_args: "DataArguments", training_args: "Seq2SeqTrainingArguments"
+) -> Dict[str, "Dataset"]:
+    if training_args.do_train:
+        if data_args.val_size > 1e-6:  # Split the dataset
+            if data_args.streaming:
+                dataset = dataset.shuffle(buffer_size=data_args.buffer_size, seed=training_args.seed)
+                val_set = dataset.take(int(data_args.val_size))
+                train_set = dataset.skip(int(data_args.val_size))
+                return {"train_dataset": train_set, "eval_dataset": val_set}
             else:
-                reorg_datasets[split_name].append(dataset_split)
-
-    return reorg_datasets
-
-
-def concat_datasets(datasets):
-    """
-    Concatenates multiple datasets into a single dataset.
-
-    It supports may-style datasets and DataPipeline from WebDataset. Currently, does not support
-    generic IterableDataset because it requires creating separate samplers.
-
-    Now only supports conctenating training datasets and assuming validation and testing
-    have only a single dataset. This is because metrics should not be computed on the concatenated
-    datasets.
-
-    Args:
-        datasets: dict of torch.utils.data.Dataset objects by split.
-
-    Returns:
-        Dict of concatenated datasets by split, "train" is the concatenation of multiple datasets,
-        "val" and "test" remain the same.
-
-        If the input training datasets contain both map-style and DataPipeline datasets, returns
-        a tuple, where the first element is a concatenated map-style dataset and the second
-        element is a chained DataPipeline dataset.
-
-    """
-    # concatenate datasets in the same split
-    for split_name in datasets:
-        if split_name != "train":
-            assert (
-                len(datasets[split_name]) == 1
-            ), "Do not support multiple {} datasets.".format(split_name)
-            datasets[split_name] = datasets[split_name][0]
+                val_size = int(data_args.val_size) if data_args.val_size > 1 else data_args.val_size
+                dataset = dataset.train_test_split(test_size=val_size, seed=training_args.seed)
+                return {"train_dataset": dataset["train"], "eval_dataset": dataset["test"]}
         else:
-            iterable_datasets, map_datasets = [], []
-            for dataset in datasets[split_name]:
-                if isinstance(dataset, wds.DataPipeline):
-                    logging.info(
-                        "Dataset {} is IterableDataset, can't be concatenated.".format(
-                            dataset
-                        )
-                    )
-                    iterable_datasets.append(dataset)
-                elif isinstance(dataset, IterableDataset):
-                    raise NotImplementedError(
-                        "Do not support concatenation of generic IterableDataset."
-                    )
-                else:
-                    map_datasets.append(dataset)
-
-            # if len(iterable_datasets) > 0:
-            # concatenate map-style datasets and iterable-style datasets separately
-            chained_datasets = (
-                ChainDataset(iterable_datasets) if len(iterable_datasets) > 0 else None
-            )
-            concat_datasets = (
-                ConcatDataset(map_datasets) if len(map_datasets) > 0 else None
-            )
-
-            train_datasets = concat_datasets, chained_datasets
-            train_datasets = tuple([x for x in train_datasets if x is not None])
-            train_datasets = (
-                train_datasets[0] if len(train_datasets) == 1 else train_datasets
-            )
-
-            datasets[split_name] = train_datasets
-
-    return datasets
-
-
-def extract_archive(from_path, to_path=None, overwrite=False):
-    """Extract archive.
-
-    Args:
-        from_path: the path of the archive.
-        to_path: the root path of the extracted files (directory of from_path)
-        overwrite: overwrite existing files (False)
-
-    Returns:
-        List of paths to extracted files even if not overwritten.
-
-    Examples:
-        >>> url = 'http://www.quest.dcs.shef.ac.uk/wmt16_files_mmt/validation.tar.gz'
-        >>> from_path = './validation.tar.gz'
-        >>> to_path = './'
-        >>> torchtext.utils.download_from_url(url, from_path)
-        >>> torchtext.utils.extract_archive(from_path, to_path)
-        >>> ['.data/val.de', '.data/val.en']
-        >>> torchtext.utils.download_from_url(url, from_path)
-        >>> torchtext.utils.extract_archive(from_path, to_path)
-        >>> ['.data/val.de', '.data/val.en']
-
-    """
-
-    if to_path is None:
-        to_path = os.path.dirname(from_path)
-
-    if from_path.endswith((".tar.gz", ".tgz")):
-        logging.info("Opening tar file {} to {}.".format(from_path, to_path))
-        with tarfile.open(from_path, "r") as tar:
-            files = []
-            for file_ in tqdm(tar):
-                file_path = os.path.join(to_path, file_.name)
-                if file_.isfile():
-                    files.append(file_path)
-                    if os.path.exists(file_path):
-                        logging.info("{} already extracted.".format(file_path))
-                        if not overwrite:
-                            continue
-                tar.extract(file_, to_path)
-            logging.info("Finished extracting tar file {}.".format(from_path))
-            return files
-
-    elif from_path.endswith(".zip"):
-        assert zipfile.is_zipfile(from_path), from_path
-        logging.info("Opening zip file {} to {}.".format(from_path, to_path))
-        with zipfile.ZipFile(from_path, "r") as zfile:
-            files = []
-            for file_ in tqdm(zfile.namelist()):
-                file_path = os.path.join(to_path, file_)
-                files.append(file_path)
-                if os.path.exists(file_path):
-                    logging.info("{} already extracted.".format(file_path))
-                    if not overwrite:
-                        continue
-                zfile.extract(file_, to_path)
-        files = [f for f in files if os.path.isfile(f)]
-        logging.info("Finished extracting zip file {}.".format(from_path))
-        return files
-
-    elif from_path.endswith(".gz"):
-        logging.info("Opening gz file {} to {}.".format(from_path, to_path))
-        default_block_size = 65536
-        filename = from_path[:-3]
-        files = [filename]
-        with gzip.open(from_path, "rb") as gzfile, open(filename, "wb") as d_file:
-            while True:
-                block = gzfile.read(default_block_size)
-                if not block:
-                    break
-                else:
-                    d_file.write(block)
-            d_file.write(block)
-        logging.info("Finished extracting gz file {}.".format(from_path))
-        return files
-
-    else:
-        raise NotImplementedError(
-            "We currently only support tar.gz, .tgz, .gz and zip achives."
-        )
-
-
-def save_frames_grid(img_array, out_path):
-    import torch
-    from PIL import Image
-    from torchvision.utils import make_grid
-
-    if len(img_array.shape) == 3:
-        img_array = img_array.unsqueeze(0)
-    elif len(img_array.shape) == 5:
-        b, t, c, h, w = img_array.shape
-        img_array = img_array.view(-1, c, h, w)
-    elif len(img_array.shape) == 4:
-        pass
-    else:
-        raise NotImplementedError(
-            "Supports only (b,t,c,h,w)-shaped inputs. First two dimensions can be ignored."
-        )
-
-    assert img_array.shape[1] == 3, "Exepcting input shape of (H, W, 3), i.e. RGB-only."
-
-    grid = make_grid(img_array)
-    ndarr = grid.permute(1, 2, 0).to("cpu", torch.uint8).numpy()
-
-    img = Image.fromarray(ndarr)
-
-    img.save(out_path)
-
-
-def uniform_frame_sampling(
-    video_path, num_frames, target_height, target_width, start_time=None, end_time=None
-):
-    cap = cv2.VideoCapture(video_path)
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_rate = cap.get(cv2.CAP_PROP_FPS)
-
-    if start_time is None:
-        start_time = 0
-    if end_time is None:
-        end_time = total_frames / frame_rate
-
-    start_frame = int(start_time * frame_rate)
-    end_frame = int(end_time * frame_rate)
-    frame_indices = list(
-        range(start_frame, end_frame + 1, (end_frame - start_frame + 1) // num_frames)
-    )
-
-    frames = []
-    for frame_index in frame_indices:
-        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frame = cv2.resize(frame, (target_width, target_height))
-        frames.append(frame)
-
-    cap.release()
-    return frames
-
-
-def head_tail_frame_sampling(
-    video_path, num_frames, target_height, target_width, start_time=None, end_time=None
-):
-    cap = cv2.VideoCapture(video_path)
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    frame_rate = cap.get(cv2.CAP_PROP_FPS)
-
-    if start_time is None:
-        start_time = 0
-    if end_time is None:
-        end_time = total_frames / frame_rate
-
-    start_frame = int(start_time * frame_rate)
-    end_frame = int(end_time * frame_rate)
-    frame_indices = (
-        [start_frame]
-        + [
-            start_frame + (end_frame - start_frame) // (num_frames - 1) * i
-            for i in range(1, num_frames - 1)
-        ]
-        + [end_frame]
-    )
-
-    frames = []
-    for frame_index in frame_indices:
-        cap.set(cv2.CAP_PROP_POS_FRAMES, frame_index)
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frame = cv2.resize(frame, (target_width, target_height))
-        frames.append(frame)
-
-    cap.release()
-    if len(frames) == 0:
-        return None
-    return torch.stack(
-        [torch.tensor(f).permute(2, 0, 1).float() for f in frames], dim=1
-    )
-
-
-def load_clip(
-    video_path,
-    num_frames,
-    target_height,
-    target_width,
-    start_time=None,
-    end_time=None,
-    sampling="headtail",
-):
-    if sampling == "headtail":
-        return head_tail_frame_sampling(
-            video_path, num_frames, target_height, target_width, start_time, end_time
-        )
-    elif sampling == "uniform":
-        return uniform_frame_sampling(
-            video_path, num_frames, target_height, target_width, start_time, end_time
-        )
-    else:
-        raise NotImplementedError
+            if data_args.streaming:
+                dataset = dataset.shuffle(buffer_size=data_args.buffer_size, seed=training_args.seed)
+            return {"train_dataset": dataset}
+    else:  # do_eval or do_predict
+        return {"eval_dataset": dataset}
--- easyai/data/datasets/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Author: watcher
-# @Created Time: 2024/6/5 下午2:15
-# @File: __init__.py
-# @Email: mlshenkai@163.com
