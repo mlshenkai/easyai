30e9ed776af4b8a4e075ab228bc60238538db86c
watcher.shen <mlshenkai@163.com>
2024-06-25 13:51:52 +0800

    fix 修正注册机制

--- easyai/models/llama3/modeling_llama3.py
+++ easyai/models/llama3/modeling_llama3.py
@@ -13,7 +13,8 @@ from typing import Optional, Tuple, List, Union
 
 from loguru import logger
 from omegaconf import DictConfig
-from transformers import PreTrainedModel, MODEL_FOR_CAUSAL_LM_MAPPING
+from transformers import PreTrainedModel
+from easyai.common.auto_constants import AutoModelEnum
 from transformers.activations import ACT2FN
 from transformers.modeling_outputs import (
     BaseModelOutputWithPast,
@@ -661,7 +662,7 @@ class Llama3Model(Llama3PreTrainedModel):
         )
 
 
-@registry.register_model("llama3-7b", Llama3Config, MODEL_FOR_CAUSAL_LM_MAPPING)
+@registry.register_model("llama3-7b", Llama3Config, AutoModelEnum.MODEL_FOR_CAUSAL_LM)
 class Llama3ModelForCausalLM(Llama3PreTrainedModel):
     _tied_weights_keys = ["lm_head.weight"]
 
@@ -830,7 +831,8 @@ class Llama3ModelForCausalLM(Llama3PreTrainedModel):
             )
         return reordered_past
 
-@registry.register_model("llama3-7b", )
+
+@registry.register_model("llama3-7b", Llama3Config, AutoModelEnum.MODEL_FOR_SEQUENCE_CLASSIFICATION)
 class Llama3ForSequenceClassification(Llama3PreTrainedModel):
     def __init__(self, config: Union[Llama3Config, dict]):
         if isinstance(config, dict):
